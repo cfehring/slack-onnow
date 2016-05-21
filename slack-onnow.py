93d592bdd482223e0620b7fda0f30bf680a03160
cfehring <cfehring@users.noreply.github.com>
2016-05-20 17:03:55 -0700

    Combined all into slack-onnow.py. Left onnowUrls.py but can delete works if you give it an argument (n)

--- slack-onnow.py
+++ slack-onnow.py
@@ -61,6 +61,8 @@ import boto3
 from base64 import b64decode
 from urlparse import parse_qs
 import logging
+import requests #Digital recommends the request lib to interact w/ apis over http. http://docs.python-requests.org/
+import sys # for sys.argv to pass arguments from command line
 
 ENCRYPTED_EXPECTED_TOKEN = "CiDwhvH0HpF1WdM/Qj2SO8kDmtA9FMF7ZIZN8ambj2J3gRKfAQEBAgB48Ibx9B6RdVnTP0I9kjvJA5rQPRTBe2SGTfGpm49id4EAAAB2MHQGCSqGSIb3DQEHBqBnMGUCAQAwYAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAyjjtXp0tXAwOk3J9wCARCAMxstVfCsQUyB8g0ZnEuRs1rb6Fbor5JusgRbHuKJc6HsWDWr2vQGnBU05JowdV1mP0L5yQ==" # Enter the base-64 encoded, encrypted Slack command token (CiphertextBlob)
 
@@ -81,6 +83,79 @@ def lambda_handler(event, context):
     user = params['user_name'][0]
     command = params['command'][0]
     channel = params['channel_name'][0]
-#    command_text = params['text'][0]
+    command_text = params['text'][0]
+#    myurl = onnowUrls()
+#    return(myurl)
+#    return "%s invoked %s in %s" % (user, command, channel)
 
-    return "%s invoked %s in %s" % (user, command, channel)
+
+
+    # initialize static variables for urls
+    epg_url = 'http://api.pac-12.com/v3/epg' #production server
+    event_url = 'http://api.pac-12.com/v3/events'  #production server
+    onnow_url = 'http://api.pac-12.com/v3/onnow'
+
+    #Get arguments from python command line and iterate through the networks
+    args = command_text.split(' ')
+    if len(args) is 1:
+        args.extend(["n", "a", "b", "l", "m", "o", "w"])
+#        args.extend(["n"])
+        ##print args
+    for i in range(len(args)):
+        if i == 0:
+            None # Don't need to do anything with the name of the script, just the arguments
+
+        else:
+            net = "%s" % (args[i])
+
+            if net == 'n':
+                networks = 88 #Pac-12 Networks
+                net_name = "NAT"
+            elif net == 'b':
+                networks = 86 #Pac-12 Bay Area
+                net_name = "BAY"
+            elif net == 'a':
+                networks = 91 #Pac-12 Arizona
+                net_name = "ARZ"
+            elif net == 'l':
+                networks = 92 #Pac-12 Los Angeles
+                net_name = "LAX"
+            elif net == 'm':
+                networks = 87 #Pac-12 Mountain
+                net_name = "MTN"
+            elif net == 'o':
+                networks = 89 #Pac-12 Oregon
+                net_name = "ORG"
+            elif net == 'w':
+                networks = 90 #Pac-12 Washington
+                net_name = "WAS"
+
+#            elif net == 'help':
+#                print "enter the first letter of each network after the script name as shown in the following examples"
+#                print "python onnow-urls.py n a b l m o w"
+#                print "python onnow-urls.py n"
+#                print "python onnow-urls.py b l"
+#                sys.exit()
+
+            ##print file # indicate which file the date will be written to
+
+            # limit results to just the next event
+            pagesize = 1
+
+            # request to p12 api for onnow data
+            onnow_get_params = {'pagesize': pagesize , 'networks': networks}
+            onnow_get_request  = requests.get(onnow_url, params=onnow_get_params)
+            onnow_get_response = onnow_get_request.json()
+
+            # request to p12 api for EPG data
+            ##epg_get_params = {'pagesize': pagesize , 'networks': networks}
+            ##epg_get_request  = requests.get(epg_url, params=epg_get_params)
+            ##epg_get_response = epg_get_request.json()
+
+            # Pull out the override_url from the onnow api
+            override_url = onnow_get_response['programs'][0]['override_url']
+            if override_url is None:
+                return_url = onnow_get_response['programs'][0]['url']
+            else:
+                return_url = override_url
+            return "{}: {}".format(net_name,return_url)
