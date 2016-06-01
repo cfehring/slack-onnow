6b0b0ac7d2ba7d1862fe6da0e6202b2421a09da1
cfehring <cfehring@users.noreply.github.com>
2016-06-01 16:48:36 -0700

    First deployent v1.0 Works if you don't include any parameters (network abbreviations) Displays help if you include the wrong parameters Also displays help if you do /onnow help

--- slack-onnow.py
+++ slack-onnow.py
@@ -1,5 +1,5 @@
 '''
-This function handles a Slack slash command and echoes the details back to the user.
+This function handles a Slack slash command named "/onnow" and echoes the details back to the Slack user.
 
 Follow these steps to configure the slash command in Slack:
 
@@ -7,12 +7,19 @@ Follow these steps to configure the slash command in Slack:
 
   2. Search for and select "Slash Commands".
 
-  3. Enter a name for your command and click "Add Slash Command Integration".
+  3. Enter a name for your command (/onnow) and click "Add Slash Command Integration".
 
   4. Copy the token string from the integration settings and use it in the next section.
 
   5. After you complete this blueprint, enter the provided API endpoint URL in the URL field.
 
+  6. Method: POST
+
+  7. Customize Name: Pac-12 API
+
+  8. Show this command in the autocomplete list (checked)
+    a. Description: returns live events currently airing on Pac-12.com
+    b. Usage hint: abbreviate networks: n a b l m o w
 
 Follow these steps to encrypt your Slack token for use in this function:
 
@@ -63,12 +70,6 @@ from urlparse import parse_qs
 import logging
 import requests #Digital recommends the request lib to interact w/ apis over http. http://docs.python-requests.org/
 import sys # for sys.argv to pass arguments from command line
-#import slackweb
-# import json
-# from urllib2 import Request, urlopen, URLError, HTTPError
-# from urllib import urlencode
-#import slacker
-
 
 ENCRYPTED_EXPECTED_TOKEN = "CiDwhvH0HpF1WdM/Qj2SO8kDmtA9FMF7ZIZN8ambj2J3gRKfAQEBAgB48Ibx9B6RdVnTP0I9kjvJA5rQPRTBe2SGTfGpm49id4EAAAB2MHQGCSqGSIb3DQEHBqBnMGUCAQAwYAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAyjjtXp0tXAwOk3J9wCARCAMxstVfCsQUyB8g0ZnEuRs1rb6Fbor5JusgRbHuKJc6HsWDWr2vQGnBU05JowdV1mP0L5yQ==" # Enter the base-64 encoded, encrypted Slack command token (CiphertextBlob)
 
@@ -78,45 +79,49 @@ expected_token = kms.decrypt(CiphertextBlob = b64decode(ENCRYPTED_EXPECTED_TOKEN
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Define the lambda function, which returns a python dict key named 'ret'
+#   that gets translated to a json formatted object named 'ret'.
+# 'ret' includes two items in a python list, which gets translated as two json
+#   arrays named 'text' and 'attachments'
+
 def lambda_handler(event, context):
     req_body = event['body']
-    params = parse_qs(req_body)
+    params = parse_qs(req_body,keep_blank_values=1)
     token = params['token'][0]
     if token != expected_token:
         logger.error("Request token (%s) does not match exptected", token)
         raise Exception("Invalid request token")
 
-    user = params['user_name'][0]
-    command = params['command'][0]
-    channel = params['channel_name'][0]
+    # could get additional info from slack if needed in the future:
+    # user = params['user_name'][0]
+    # command = params['command'][0]
+    # channel = params['channel_name'][0]
+
     command_text = params['text'][0]
-#    myurl = onnowUrls()
-#    return(myurl)
-#    return "%s invoked %s in %s" % (user, command, channel)
 
+    # Split the arguments passed from the user into a list of networks named 'args'
+    if command_text:
+        args = command_text.split(' ')
 
+    # If no arguments are included, return data for all networks
+    else:
+        args = ["n", "a", "b", "l", "m", "o", "w"]
 
-    # initialize static variables for urls
+    # initialize static variables for urls - note version 3
     epg_url = 'http://api.pac-12.com/v3/epg' #production server
     event_url = 'http://api.pac-12.com/v3/events'  #production server
     onnow_url = 'http://api.pac-12.com/v3/onnow'
 
-    #Get arguments from python command line and iterate through the networks
-    args = command_text.split(' ')
-    if len(args) is 0:
-        args.extend(["n", "a", "b", "l", "m", "o", "w"])
-#        args.extend(["n"])
-        ##print args
-
+    # initialize the dictionary named 'ret', formatted the way slack likes the JSON
     ret = {}
     ret["text"] = "Currently airing on Pac-12.com:"
     ret["attachments"] = []
 
+    # Iterate through the list of 'args' (networks)
+    # and set up the "networks" variable to pass to the Pac-12 API
+    # and the "net_name" variable to include as text back to the user
     for i in range(len(args)):
-#        if i == 0:
-#            None # Don't need to do anything with the name of the script, just the arguments
 
-#        else:
         net = "{}".format(args[i])
 
         if net == 'n':
@@ -141,14 +146,20 @@ def lambda_handler(event, context):
             networks = 90 #Pac-12 Washington
             net_name = "WAS"
 
-    #            elif net == 'help':
-    #                print "enter the first letter of each network after the script name as shown in the following examples"
-    #                print "python onnow-urls.py n a b l m o w"
-    #                print "python onnow-urls.py n"
-    #                print "python onnow-urls.py b l"
-    #                sys.exit()
+#        elif net == 'help':
+        # If network letters are entered incorrectly, display help and exit
+        # Also works with /onnow help    -or-   /onnow ?
+        else:
+            help1 = "Separating with spaces, enter the first letter of each network after /onnow, as shown below:"
+            help2 = "/onnow n"
+            help3 = "/onnow b l"
+            help4 = "/onnow n a b l m o w"
 
-        ##print file # indicate which file the date will be written to
+            message = "{}\n{}\n{}\n{}".format(help1,help2,help3,help4)
+
+            ret["attachments"].append({"text": message})
+            return ret
+            sys.exit
 
         # limit results to just the next event
         pagesize = 1
@@ -158,28 +169,18 @@ def lambda_handler(event, context):
         onnow_get_request  = requests.get(onnow_url, params=onnow_get_params)
         onnow_get_response = onnow_get_request.json()
 
-        # request to p12 api for EPG data
-        ##epg_get_params = {'pagesize': pagesize , 'networks': networks}
-        ##epg_get_request  = requests.get(epg_url, params=epg_get_params)
-        ##epg_get_response = epg_get_request.json()
-
-        # Pull out the override_url from the onnow api
+        # Pull out the override_url from the onnow call to the pac-12 api
         override_url = onnow_get_response['programs'][0]['override_url']
+
+        # If the override_url doesn't exist, use the 'url'
         if override_url is None:
             return_url = onnow_get_response['programs'][0]['url']
         else:
             return_url = override_url
-    #    eol = \n
 
+        #Format the response returned to Slack
         link = "{}: <{}>".format(net_name,return_url)
 
-    ##############
-
-        # ret = {}
-        # ret["text"] = "Currently airing on Pac-12.com:"
-        # ret["attachments"] = []
-        # ret["attachments"].append({"fallback": "Hmm something didnt work right"})
-        # ret["attachments"].append({"color": "#36a64f"})
         ret["attachments"].append({"text": link})
 
     return ret
